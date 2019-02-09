0aa7773021a5b5d7c880544aaaf62ec0bfc9363a
Mamontt90 <45282544+Mamontt90@users.noreply.github.com>
2019-02-09 15:28:00 +0300

    Update 2019_02_09_PWM_buzzer.ino
    В этом измение я изменил задержку с delay() на конструкцию с millis() ,эта конструкция позволяет тормозить только заданную часть программы а,не всю как delay().

--- 2019_02_09_PWM_buzzer.ino
+++ 2019_02_09_PWM_buzzer.ino
@@ -1,14 +1,19 @@
 int pwm;
+unsigned long time_chash=0;
 void setup() 
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
+    pinMode(A2,INPUT);
+    pinMode(9,OUTPUT);
 }
-
 void loop() 
 {
-  pwm = analogRead(A2);
-  pwm=map(pwm,0,1023,0,255);
-  Serial.println(pwm);
-  analogWrite(10,pwm);
-  delay(100);
+    if(millis()-time_chash == 100)
+    {
+        time_chash=millis();
+        pwm = analogRead(A2);
+        pwm=map(pwm,0,1023,0,255);
+        Serial.println(pwm);
+        analogWrite(9,pwm);
+    }
 }
